3bbaea067a2e86fd716624fa9b90429c84dafaf7
ShemiPeretz <87975112+ShemiPeretz@users.noreply.github.com>
2023-01-14 12:53:09 +0200

    Update Metric.py
    new Metric class

--- IndexUtils/Metric.py
+++ IndexUtils/Metric.py
@@ -1,6 +1,5 @@
-import numpy as np
 import scipy
-
+import numpu as np
 
 class Metric:
   def __init__(self, inverted_index):
@@ -20,6 +19,7 @@ class Metric:
     -------
     dict, key --> (doc_id, term), value --> tfidf
     """
+    term_vector = list(self.inverted_index.term_total.keys())  
     eps = 10 ** -9
     docs = {}
     tokens = list(set(query))
@@ -27,12 +27,43 @@ class Metric:
       byte_pl = self.inverted_index.get_byte_pl(word=token)
       
       pl = self.inverted_index.byte_pl_to_list(byte_pl)
-      for doc_id,tf in pl:
-        key = (doc_id,token)
+      for doc_id, tf in pl:
+        # key = (doc_id,token)
+        norm_tf = tf / self.inverted_index.doc2len_and_tfidf_size[doc_id][1]
         idf = np.log(self.inverted_index.N / self.inverted_index.df[token] + eps)
-        tfidf = tf * idf 
-        docs[key] = tfidf
+        tfidf = norm_tf * idf 
+        # docs[key] = tfidf
+        token_index = term_vector.index(token)
+        docs.setdefault(doc_id, [])
+        docs[doc_id].append((token_index, tfidf))
     return docs
+
+  def spars_matrix_to_dict(self, matrix):
+    
+    indices = matrix.nonzero() 
+    # Get the values of the non-zero elements
+    values = matrix.data
+    # Zip the indices and values into a list of tuples
+    non_zero_elements = list(zip(indices[0], indices[1], values))
+    docs = set([non_zero_elements[i][0] for i in range(len(non_zero_elements))])
+    docs_tfidf = dict.fromkeys(docs,None)
+    for doc,term_index, tfidf in non_zero_elements:
+      if docs_tfidf[doc] is None: 
+        docs_tfidf[doc] = [(term_index,tfidf)]
+      else:
+        docs_tfidf[doc].append((term_index,tfidf))
+    return docs_tfidf
+
+  def tfidf_vec_size(self,doc_id):
+    doc_stasts = self.inverted_index.doc_stats[doc_id]
+    vec_size = 0 
+    for term,tf in doc_stasts.items():
+      term_df = self.inverted_index.df[term]
+      idf = np.log(self.inverted_index.N / term_df)
+      tfidf = tf * idf
+      vec_size += tfidf ** 2
+    vec_size = np.sqrt(vec_size)
+    return vec_size
 
 
   def get_tfidf_matrix(self,query):
@@ -50,43 +81,23 @@ class Metric:
 
 
     """
-    total_vocab_size = len(self.inverted_index.term_total)
-    term_vector = list(self.inverted_index.term_total.keys())  
-    candidates_scores = self.get_candidate_docs(query) 
-    print(f"candidates_scores = {candidates_scores}")
-    unique_candidates = np.unique([doc_id for doc_id, freq in candidates_scores.keys()])
-    print(f"unique_candidates = {unique_candidates}")
-    # D = np.zeros((len(unique_candidates), total_vocab_size))
-    # D = pd.DataFrame(D)
-    
-    # D.index = unique_candidates
-    # D.columns = self.inverted_index.term_total.keys()
-    total_candidates = len(unique_candidates)
-    matrix = scipy.sparse.lil_matrix((total_candidates, total_vocab_size))
-    for key, tfidf in candidates_scores.items():
-      doc_id, term = key 
-      try:
-        term_index = term_vector.index(term) 
-        matrix[doc_id, term_index] = tfidf                    
-      except:
-          pass 
-    matrix = matrix.tocsr()
-    indices = matrix.nonzero()
-
-    # Get the values of the non-zero elements
-    values = matrix.data
-
-    # Zip the indices and values into a list of tuples
-    non_zero_elements = list(zip(indices[0], indices[1], values))
-    docs = set([non_zero_elements[i][0] for i in range(len(non_zero_elements))])
-    docs_tfidf = dict.fromkeys(docs,None)
-    for doc,term_index, tfidf in non_zero_elements:
-      if docs_tfidf[doc] is None: 
-        docs_tfidf[doc] = [(term_index,tfidf)]
-      else:
-        docs_tfidf[doc].append((term_index,tfidf))
-
-    return docs_tfidf
+    # total_vocab_size = len(self.inverted_index.term_total)
+    candidates_scores = self.get_candidate_docs(query)
+    # print(f"candidates_scores = {candidates_scores}")
+    # unique_candidates = np.unique([doc_id for doc_id, term in candidates_scores.keys()])
+    # print(f"unique_candidates = {unique_candidates}")
+    # total_candidates = len(unique_candidates)
+    # matrix = scipy.sparse.lil_matrix((total_candidates, total_vocab_size))
+    # for key, tfidf in candidates_scores.items():
+    #   doc_id, term = key 
+    #   try:
+    #     term_index = term_vector.index(term) 
+    #     matrix[doc_id, term_index] = tfidf                    
+    #   except:
+    #       pass 
+    # matrix = matrix.tocsr()
+    # doc_tfidf = self.spars_matrix_to_dict(matrix)
+    return candidates_scores
 
   def get_top_n(self, score_dict,N=3):
     """ 
